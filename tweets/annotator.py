861281106c446c42ca40d56fe9b5603aaa5d92d3
Tyler Newman <tnewman16@gmail.com>
2018-07-12 21:06:13 -0400

    Refactors Tweet Annotator Adds a skip tweet button Can now filter based on sentiment

--- tweets/annotator.py
+++ tweets/annotator.py
@@ -37,20 +37,21 @@ def remove_already_seen(original_df, seen_df):
     return original_df[~original_df.index.isin(seen_df.index.tolist())]
 
 
-def get_random_tweet(df):
+def get_random_tweet(df, sentiments=['"negative"', '"positive"', '"neutral"']):
     # subset to rows that do not already have a mask
-    new_df = df[df['mask'].str.len() == 0].reset_index()
+    new_df = df[(df['mask'].str.len() == 0) & (
+        df['sentiment'].isin(sentiments))].reset_index()
     return new_df.sample(n=1)
 
 
-def on_create_mask(row, mask, mask_file_name):
+def on_create_mask(row, mask, mask_file_name, sentiments):
     global original_df
     global mask_df
     global unseen_df
     row['mask'] = [mask]
     mask_df = mask_df.append(row)
     save_mask_df(mask_df, mask_file_name)
-    reset_display(mask_file_name)
+    reset_display(mask_file_name, sentiments)
 
 
 def display_buttons(row, create_mask_callback, mask_file_name, sentiments=['"negative"', '"positive"', '"neutral"']):
@@ -77,16 +78,23 @@ def display_buttons(row, create_mask_callback, mask_file_name, sentiments=['"neg
         flex_flow='row wrap', justify_content='center'))
     display(wrapper)
     create_button = Button(description='Create Mask', layout=Layout(
-        width='auto', height='auto', padding='2px 15px 2px 15px'))
+        width='auto', height='auto', padding='2px 15px 2px 15px', margin='10px'))
     create_button.style.button_color = '#5EACF9'
-    def callback(b):
+    skip_button = Button(description='Skip Tweet', layout=Layout(
+        width='auto', height='auto', padding='2px 15px 2px 15px', margin='10px'))
+    skip_button.style.button_color = '#F9DC5C'
+    def create_callback(b):
         mask = []
         for button in buttons:
             mask.append(button.style.button_color is not None)
         clear_output()
-        create_mask_callback(row, mask, mask_file_name)
-    create_button.on_click(callback)
-    create_wrapper = HBox([create_button], layout=Layout(
+        create_mask_callback(row, mask, mask_file_name, sentiments)
+    def skip_callback(b):
+        clear_output()
+        reset_display(mask_file_name, sentiments)
+    create_button.on_click(create_callback)
+    skip_button.on_click(skip_callback)
+    create_wrapper = HBox([create_button, skip_button], layout=Layout(
         margin='20px', justify_content='center'))
     display(create_wrapper)
     done_texts = []
@@ -97,19 +105,19 @@ def display_buttons(row, create_mask_callback, mask_file_name, sentiments=['"neg
     display(done_wrapper)
 
 
-def reset_display(mask_file_name):
+def reset_display(mask_file_name, sentiments):
     global original_df
     global mask_df
     global unseen_df
     unseen_df = remove_already_seen(original_df, mask_df)
-    tweet = get_random_tweet(unseen_df)
-    display_buttons(tweet, on_create_mask, mask_file_name)
+    tweet = get_random_tweet(unseen_df, sentiments)
+    display_buttons(tweet, on_create_mask, mask_file_name, sentiments)
 
 
-def run_annotator(original_file_name, mask_file_name):
+def run_annotator(original_file_name, mask_file_name, sentiments=['"negative"', '"positive"', '"neutral"']):
     global original_df
     global mask_df
     global unseen_df
     original_df = init_original_df(original_file_name)
     mask_df = init_mask_df(mask_file_name)
-    reset_display(mask_file_name)
+    reset_display(mask_file_name, sentiments)
